6470fd42a4a574926f26743ceb727fa540e237f4
Massimo 'Max' Mucci <max.i8nhj@gmail.com>
2023-04-12 14:58:58 -0500

    Update EthConfig.h

--- EthConfig.h
+++ EthConfig.h
@@ -1,7 +1,7 @@
 // ETHERNET set-up
 // #define MAX_SOCK_NUM 1
 byte mac[] = { 0xA8, 0x61, 0x0A, 0xAE, 0x98, 0x37 };  // Enter a MAC address for your controller.
-IPAddress FRStackPC(192, 168, 1, 139);                // use the numeric IP instead of the name for the server:
+IPAddress FRStackPC(192, 168, 1, 139);                // use the numeric IP instead for the FRstack PC on your lan.
 IPAddress myIP(192, 168, 1, 237);
 IPAddress myDns(192, 168, 1, 254);
 IPAddress myGateway(192, 168, 1, 254);
